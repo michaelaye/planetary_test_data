1c86cf0da94f400d424e3306d424d0f36a8b9cf8
K.-Michael Aye <kmichael.aye@gmail.com>
2019-07-04 16:41:58 -0600

    add get_mission_data to main package namespace

--- planetary_test_data/__init__.py
+++ planetary_test_data/__init__.py
@@ -5,6 +5,6 @@ __email__ = 'contact@planetarypy.com'
 __version__ = '0.4.0'
 
 
-from .planetary_test_data import PlanetaryTestDataProducts
+from .planetary_test_data import PlanetaryTestDataProducts, get_mission_data
 
 __all__ = ['PlanetaryTestDataProducts']
